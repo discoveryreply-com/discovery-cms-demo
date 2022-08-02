d9c775cbfa30cd48da5d0b912135f01875a3c18d
Paolo Innocenti <innocentipaolo1@gmail.com>
2022-08-02 10:26:36 +0200

    added React to UndefinedComponent.jsx

--- lib/src/components/UndefinedComponent.jsx
+++ lib/src/components/UndefinedComponent.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export default function UndefinedComponent({ componentType }) {
     return (
         <div style={{ color: 'red', marginTop: '32px', marginBottom: '32px', textAlign: 'center', fontSize: '2rem' }}>
