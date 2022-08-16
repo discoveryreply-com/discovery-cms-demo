dd0bc8cb54992eee7dbae5cfa1f3a06c1914a5e5
Paolo Innocenti <innocentipaolo1@gmail.com>
2022-08-16 12:39:30 +0200

    the UndefinedComponent.jsx now also logs an error in the console

--- lib/src/components/UndefinedComponent.jsx
+++ lib/src/components/UndefinedComponent.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 export default function UndefinedComponent({ componentType }) {
+    console.error(`Unregistered component type: ${componentType}`);
+
     return (
         <div style={{ color: 'red', marginTop: '32px', marginBottom: '32px', textAlign: 'center', fontSize: '2rem' }}>
             Unregistered component type: {componentType}
