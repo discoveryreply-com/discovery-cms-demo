62832323d69135835110d13052159e3b098b3626
Paolo Innocenti <innocentipaolo1@gmail.com>
2022-09-08 10:07:07 +0200

    added null check to avoid errors

--- demo-nextjs/components/Dynamic/EditorChoice.js
+++ demo-nextjs/components/Dynamic/EditorChoice.js
@@ -14,21 +14,27 @@ export default function EditorChoice({ componentId }) {
                 {componentData.headline}
             </div>
             <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-2 justify-items-center place-content-center mr-10 ml-10 md:mr-5 md:ml-5 xl:mr-20 xl:ml-20">
-                <EditorChoiceItem
-                    title={componentData.item1.metadata?.title ?? ''}
-                    description={componentData.item1.metadata?.description ?? ''}
-                    image={componentData.item1.url ?? '/no-image.jpg'}
-                />
-                <EditorChoiceItem
-                    title={componentData.item2.metadata?.title ?? ''}
-                    description={componentData.item2.metadata?.description ?? ''}
-                    image={componentData.item2.url ?? '/no-image.jpg'}
-                />
-                <EditorChoiceItem
-                    title={componentData.item3.metadata?.title ?? ''}
-                    description={componentData.item3.metadata?.description ?? ''}
-                    image={componentData.item3.url ?? '/no-image.jpg'}
-                />
+                {componentData.item1 && (
+                    <EditorChoiceItem
+                        title={componentData.item1.metadata?.title ?? ''}
+                        description={componentData.item1.metadata?.description ?? ''}
+                        image={componentData.item1.url ?? '/no-image.jpg'}
+                    />
+                )}
+                {componentData.item2 && (
+                    <EditorChoiceItem
+                        title={componentData.item2.metadata?.title ?? ''}
+                        description={componentData.item2.metadata?.description ?? ''}
+                        image={componentData.item2.url ?? '/no-image.jpg'}
+                    />
+                )}
+                {componentData.item3 && (
+                    <EditorChoiceItem
+                        title={componentData.item3.metadata?.title ?? ''}
+                        description={componentData.item3.metadata?.description ?? ''}
+                        image={componentData.item3.url ?? '/no-image.jpg'}
+                    />
+                )}
             </div>
         </div>
     );
