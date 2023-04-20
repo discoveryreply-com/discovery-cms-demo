12d810d1a054a6510bcce3ab04839380e3e0cd68
Paolo Innocenti <p.innocenti@reply.it>
2023-04-20 09:32:32 +0200

    added example for PreviewModeAlert

---
+++ demo-nextjs/components/PreviewModeAlert.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+export default function PreviewModeAlert() {
+    return (
+        <div className="py-2 text-sm text-center bg-black text-white">
+            This is page is displaying preview data.{' '}
+            <Link
+                href="/api/exit-preview"
+                className="font-bold underline transition-colors duration-200 hover:text-cyan"
+                prefetch={false}
+            >
+                Click here
+            </Link>
+            {' '}
+            to exit preview mode.
+        </div>
+    );
+}
