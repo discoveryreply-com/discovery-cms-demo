64254157a98c47c6b00a7b73524654444427d625
Paolo Innocenti <p.innocenti@reply.it>
2023-05-30 12:18:31 +0200

    added env variables for nextjs SSR on amplify

--- demo-nextjs/next.config.js
+++ demo-nextjs/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
     reactStrictMode: true,
     assetPrefix: process.env.ASSET_PREFIX ?? '',
+    env: {
+        DISCOVERY_PREVIEW_TOKEN: process.env.DISCOVERY_PREVIEW_TOKEN,
+        NEXT_PUBLIC_DISCOVERY_API_ROOT: process.env.NEXT_PUBLIC_DISCOVERY_API_ROOT,
+        NEXT_PUBLIC_DISCOVERY_API_TOKEN: process.env.NEXT_PUBLIC_DISCOVERY_API_TOKEN,
+        NEXT_PUBLIC_PROPERTY_TITLE: process.env.NEXT_PUBLIC_PROPERTY_TITLE
+    }
 };
 
 module.exports = nextConfig;
