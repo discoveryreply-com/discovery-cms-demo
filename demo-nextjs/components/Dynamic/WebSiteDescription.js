d11c5a098ca72095b65725bcfa6860fcb46da827
Gianluca Mariani <gi.mariani@reply.it>
2023-12-13 17:57:00 +0100

    added background to headline

--- demo-nextjs/components/Dynamic/WebSiteDescription.js
+++ demo-nextjs/components/Dynamic/WebSiteDescription.js
@@ -1,6 +1,20 @@
 import { useComponentData } from '@discoverycms/connector';
 import useLayoutPalette from '../../hooks/useLayoutPalette';
 
+function hexToRgba(hex, alpha) {
+    // Remove '#' if present
+    hex = hex.replace('#', '');
+
+    // Parse the hexadecimal values for red, green, and blue
+    const r = parseInt(hex.substring(0, 2), 16);
+    const g = parseInt(hex.substring(2, 4), 16);
+    const b = parseInt(hex.substring(4, 6), 16);
+
+    // Construct the RGBA color code
+    return `rgba(${r}, ${g}, ${b}, ${alpha})`;
+}
+
+
 export default function WebSiteDescription({ componentId }) {
     const componentData = useComponentData(componentId);
     const colorPalette = useLayoutPalette();
@@ -11,6 +25,7 @@ export default function WebSiteDescription({ componentId }) {
 
     const textColor = colorPalette.secondary ?? 'white';
 
+    const rgbaColor = hexToRgba(colorPalette.primary, 0.7);
     return (
         <div data-discovery-id={componentId}>
             <div
@@ -24,7 +39,7 @@ export default function WebSiteDescription({ componentId }) {
                 <div className={`flex justify-evenly w-1/2 h-96`}>
                     <div className="relative z-0 ml-40">
                         <main className="max-w-7xl ">
-                            <div className="text-center lg:text-left mt-60 mb-16">
+                            <div className="text-center lg:text-left mt-60 mb-16" style={{ background: rgbaColor, padding: `10px`}}>
                                 <h1 className="tracking-tight font-bold text-5xl md:text-6xl">
                                     <span className="block" style={{ color: textColor }}>
                                         {componentData.headline ?? ''}{' '}
