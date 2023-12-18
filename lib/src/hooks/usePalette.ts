f153813b1d8b44ac87a995686d2eae03bfc3730e
Pietro Iglio <p.iglio@reply.it>
2023-12-18 10:33:06 +0100

    fix

--- lib/src/hooks/usePalette.ts
+++ lib/src/hooks/usePalette.ts
@@ -4,7 +4,7 @@ import { getDiscoveryCms } from '../setup';
 export default function usePalette() {
     const data = useContext(getDiscoveryCms().getContext());
     if (data !== null) {
-        return (data.layout === undefined) ? data.details : data.layout;
+        return (data.layout === undefined) ? data.details : data.layout.details;
     } else {
         return null;
     }
