9d86192732cb7ab79eb2f683be74211d48e373d0
Pietro Iglio <p.iglio@reply.it>
2024-02-08 11:33:39 +0100

    Fix: componente non funzionava con le immagini croppate

--- demo-nextjs/components/Dynamic/Cta5.js
+++ demo-nextjs/components/Dynamic/Cta5.js
@@ -4,12 +4,12 @@ import Link from 'next/link';
 function GridColumn({ title, description, buttonUrl, buttonText, textColor, buttonTextColor, buttonBackgroundColor }) {
     return (
         <div className="flex flex-col items-center p-6" style={{ color: textColor }}>
-            {title && <h1 className="text-xl font-bold m-3">{title}</h1>}
-            {description && <p className="text-md m-3">{description}</p>}
+            {title && <h1 className="m-3 text-xl font-bold">{title}</h1>}
+            {description && <p className="m-3 text-md">{description}</p>}
             {buttonUrl && buttonText && (
                 <Link href={buttonUrl}>
                     <div
-                        className="flex items-center justify-center w-full px-4 py-2 text-md font-semibold rounded-full m-2"
+                        className="flex items-center justify-center w-full px-4 py-2 m-2 font-semibold rounded-full text-md"
                         style={{
                             color: buttonTextColor,
                             backgroundColor: buttonBackgroundColor,
@@ -59,7 +59,7 @@ export default function Cta5({ componentId }) {
             data-discovery-id={componentId}
             className="grid grid-cols-3 p-20"
             style={{
-                backgroundImage: `url(${backgroundImageUrl})`,
+                backgroundImage: `url('${backgroundImageUrl}')`,
                 backgroundRepeat: 'no-repeat',
                 backgroundSize: 'cover',
             }}
